59d57f7cf03ea63ec595fea2c0ecc62750509d81
Alejandro Leiva <gloob@litio.org>
2012-07-08 17:06:52 +0200

    First version of ZipPlugin for iOS.

---
+++ iOS/Zip/ZipPlugin.m
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2012 by Emergya
+ *
+ * Author: Alejandro Leiva <aleiva@emergya.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import "ZipPlugin.h"
+
+#import "ZipFile.h"
+#import "ZipException.h"
+#import "FileInZipInfo.h"
+#import "ZipWriteStream.h"
+#import "ZipReadStream.h"
+
+@implementation ZipPlugin
+
+- (void) info:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
+{
+    NSString* callbackId = [arguments pop];
+    VERIFY_ARGUMENTS(arguments, 1, callbackId)
+    
+    NSString* source = [arguments objectAtIndex:0];
+    
+    CDVPluginResult* result = nil;
+    NSString* jsString = nil;
+    
+    @try {
+        ZipFile *unzipFile = [[ZipFile alloc] initWithFileName:source mode:ZipFileModeUnzip];
+        NSArray *infos = [unzipFile listFileInZipInfos];
+        
+        for (FileInZipInfo *info in infos) {
+            NSLog(@"FILE - %@ %@ %d (%d)", info.name, info.date, info.size, info.level);
+        }
+
+		for (FileInZipInfo *info in infos) {
+			NSString *fileInfo= [NSString stringWithFormat:@"- %@ %@ %d (%d)", info.name, info.date, info.size, info.level];
+		}
+        
+        NSInteger entries = [unzipFile numFilesInZip];
+        NSLog(@"Entries: %d", entries);
+        
+        [unzipFile close];
+    }
+    @catch (ZipException *ze) {
+        NSLog(@"ZipException caught: %d - %@", ze.error, [ze reason]);
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT messageAsString:[ze reason]];
+        jsString = [result toErrorCallbackString:callbackId];
+    }
+    @catch (id e) {
+        NSLog(@"Exception caught: %@ - %@", [[e class] description], [e description]);
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT messageAsString:[e description]];
+        jsString = [result toErrorCallbackString:callbackId];
+    }
+    @finally {
+        [self writeJavascript: jsString];
+    }
+   
+}
+
+- (void) compress:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+{
+    
+}
+
+- (void) uncompress:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
+{
+    NSString* callbackId = [arguments pop];
+    VERIFY_ARGUMENTS(arguments, 1, callbackId)
+    
+    CDVPluginResult* result = nil;
+    NSString* jsString = nil;
+    
+    NSString* source = [arguments objectAtIndex:0];
+    NSString* target = [arguments objectAtIndex:1];
+    NSLog(@"source: %@ target: %@", source, target);
+    
+    [self writeJavascript: jsString];
+}
+
+@end
---
+++ iOS/Zip/assets/www/main.js
@@ -0,0 +1,111 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+function fail(error) {
+	console.log(error.code);
+}
+
+var deviceInfo = function() {
+    document.getElementById("platform").innerHTML = device.platform;
+    document.getElementById("version").innerHTML = device.version;
+    document.getElementById("uuid").innerHTML = device.uuid;
+    document.getElementById("name").innerHTML = device.name;
+    document.getElementById("width").innerHTML = screen.width;
+    document.getElementById("height").innerHTML = screen.height;
+    document.getElementById("colorDepth").innerHTML = screen.colorDepth;
+};
+
+function successListener (msg) {
+	console.log("isFile: " + msg.isFile);
+	console.log("isDirectory: " + msg.isDirectory);
+	console.log("name: " + msg.name);
+	console.log("fullPath: " + msg.fullPath);
+	console.log("completed: " + msg.completed);
+	console.log("progress: " + msg.progress);
+}
+
+function info() {
+    window.plugins.Zip.info("/Users/aleiva/Library/Application Support/iPhone Simulator/4.3.2/Applications/FF1D8AA6-5D7F-4684-B3A5-28B8245F581E/Documents/test.zip");
+}
+
+function uncompressFromSDCard() {
+	window.plugins.Zip.uncompress("file:///mnt/sdcard/test.zip", "", successListener, function(){});
+}
+
+function uncompressFromURL() {
+
+	var url = "http://10.42.0.1/test.zip";
+	var targetName = "/test.zip";
+
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+
+		fileSystem.root.getFile(targetName, {create: true, exclusive: false}, function (fileEntry) {
+
+			var localPath = fileEntry.fullPath;
+			
+			/*
+			if (device.platform === "Android" && localPath.indexof("file://") === 0) {
+				localPath = localPath.substring(7);
+			}
+			*/
+	
+			var fileTransfer = new FileTransfer();
+	
+			fileTransfer.download(
+				url,
+				localPath,
+				function (entry) {
+					console.log("download complete: " + entry.fullPath);
+					console.log("+ info: " + entry);
+				},
+				function (error) {
+					console.log("download error source " + error.source);
+					console.log("download error target " + error.target);
+					console.log("upload error code" + error.code);
+				}
+			);
+		}, fail);
+	}, fail);
+
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+
+		fileSystem.root.getFile(targetName, null, function (fileEntry) {
+
+			var localPath = fileEntry.fullPath;
+			var zip = window.plugins.Zip;
+			zip.uncompress(localPath, "/tmp", successListener, function (){});
+
+		}, fail);
+
+	}, fail);
+
+	
+}
+
+function compress() {
+
+}
+
+function init() {
+    // the next line makes it impossible to see Contacts on the HTC Evo since it
+    // doesn't have a scroll button
+    // document.addEventListener("touchmove", preventBehavior, false);
+    document.addEventListener("deviceready", deviceInfo, true);
+}
+
